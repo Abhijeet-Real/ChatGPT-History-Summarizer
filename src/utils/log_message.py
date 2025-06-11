07dccd460c7f635129cf4c5252cf74348bfd0e75
Abhijeet-Real <abhijeet1472@gmail.com>
2025-06-11 21:17:05 +0530

    Change log file name from 'summary_log.txt' to 'app.log' for consistency

--- src/utils/log_message.py
+++ src/utils/log_message.py
@@ -1,7 +1,7 @@
 import os, time
 
 def log_message(folder, message):
-    log_path = os.path.join(folder, "summary_log.txt")
+    log_path = os.path.join(folder, "app.log")
     timestamp = time.strftime("%Y-%m-%d %H:%M:%S")
     with open(log_path, "a", encoding="utf-8") as f:
         f.write(f"[{timestamp}] {message}\n")
